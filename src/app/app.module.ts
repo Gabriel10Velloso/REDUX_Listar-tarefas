6ec71cacc815cac78cde70fd7702d279b2b8a642
Gabriel Albuquerque <gabriel.albuquerque@bit.com.br>
2020-02-14 16:34:27 -0300

    Definiendo el AppState de nuestra aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,13 @@ import { TodosItemComponent } from './todo/todos-item/todos-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todo/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +29,14 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/model/todo.model.ts
@@ -0,0 +1,12 @@
+export class Todo {
+
+    public id: number;
+    public texto: string;
+    public completo: boolean;
+
+    constructor(texto: string){
+        this.texto = texto.charAt(0).toUpperCase() + texto.slice(1);
+        this.completo = false;
+        this.id = Math.random();
+    }
+}
